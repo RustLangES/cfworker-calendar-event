c10f26469105be4a19fe6b154e8550fdf463e7a8
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-03 21:49:26 -0400

    chore: get secrets

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-use worker::{
-    event, Env, ScheduleContext, ScheduledEvent,
-};
+use worker::{console_error, event, Env, ScheduleContext, ScheduledEvent};
 
 mod cangrebot;
 
@@ -11,4 +9,19 @@ pub async fn main(_e: ScheduledEvent, env: Env, _ctx: ScheduleContext) {
     std::panic::set_hook(Box::new(|info: &std::panic::PanicInfo| {
         console_error!("{info}")
     }));
+
+    let Ok(calendar_api) = env.secret("GOOGLE_APIKEY").map(|e| e.to_string()) else {
+        console_error!("Calendar Google API Secret not found");
+        return;
+    };
+
+    let Ok(calendar_id) = env.secret("GOOGLE_CALENDAR_ID").map(|e| e.to_string()) else {
+        console_error!("Calendar Google API Secret not found");
+        return;
+    };
+
+    let Ok(endpoint) = env.secret("ENDPOINT").map(|e| e.to_string()) else {
+        console_error!("Calendar Google API Secret not found");
+        return;
+    };
 }
